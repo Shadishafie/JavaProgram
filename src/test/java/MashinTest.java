a34e0c2739f9d232ae0bdb6f742204356d498fea
ShadiShafie <Shadishafie96@gmail.com>
2023-08-22 15:12:16 +0430

    Mashintest created

---
+++ src/test/java/MashinTest.java
@@ -0,0 +1,47 @@
+import static java.lang.System.out;
+
+/**
+ * Created by lenovo on 8/22/2023.
+ */
+public class MashinTest {
+    class Mashin {
+
+
+        boolean AyaDarMashinbazAst;
+        boolean AyaMashinRoshanAst;
+
+        void Roshan() {
+            out.println("Mashin Roshan Ast");
+            AyaMashinRoshanAst = true;
+        }
+
+        void Xamoosh() {
+            out.println("Mashin Xamoosh Ast");
+            AyaMashinRoshanAst = false;
+        }
+
+        void BazKardaneDar() {
+            out.println("Dar baz Ast");
+            AyaDarMashinbazAst = true;
+        }
+
+
+        void BastaneDar() {
+            out.println("Dar Baste  Ast");
+            AyaDarMashinbazAst = false;
+        }
+
+        void HarekatKon() {
+            //Mashin daresh bayad baste bashe va roshan bashe
+            if (!AyaDarMashinbazAst && AyaMashinRoshanAst) {
+                out.println("HarekatKon");
+
+            } else {
+                out.println("HarekatNakon");
+            }
+
+        }
+
+    }
+}
+
